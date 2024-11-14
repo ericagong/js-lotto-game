6bd5022afb1dded6fe8f02107098b834193a4642
Erica <48196721+ericagong@users.noreply.github.com>
2024-11-14 09:51:55 +0900

    feat: 로또 구입 금액 읽어와 로또 발행 처리

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,40 @@
+// web
+
+import Lottos from './domain/models/service/Lottos/index.js';
+
+console.log('connected');
+
+const $priceInput = document.querySelector('#price-input');
+const $priceSubmitButton = document.querySelector('#price-submit-button');
+
+let lottos = [];
+const step1 = (budget) => {
+    try {
+        lottos = Lottos.issue(budget);
+        const issuedCount = lottos.length;
+        console.log(lottos);
+        console.log(issuedCount);
+    } catch (error) {
+        window.alert(error.message);
+        $priceInput.value = '';
+    }
+};
+
+const onPriceSubmit = () => {
+    const budget = Number($priceInput.value);
+    step1(budget);
+};
+
+$priceSubmitButton.addEventListener('click', onPriceSubmit);
+
+const toggleDisplay = (element) => {
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+    } else {
+        element.style.display = 'none';
+    }
+};
+
 // const $showResultButton = document.querySelector('.open-result-modal-button');
 // const $modalClose = document.querySelector('.modal-close');
 // const $modal = document.querySelector('.modal');
